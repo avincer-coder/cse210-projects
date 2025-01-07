26fda0141b8ad6fc0a16b88c641e0d926cea0f0d
avincer-coder <gar23078@byui.edu>
2025-01-07 12:52:18 -0600

    First change

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("What is your first name?");
+        
     }
 }
